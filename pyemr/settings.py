4257a7a15855b89255acb0054ea76bd74aee5d9c
Rachel Suk <rachelsuk97@gmail.com>
2021-03-12 22:16:22 -0800

    made some changes

--- pyemr/settings.py
+++ pyemr/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w+9gm3v=zz9a!yur8%o+!!i83zxlp-u^-au8__2m#pbsu%ua6)'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'blooming-dusk-84917.herokuapp.com'
