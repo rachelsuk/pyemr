7133740a6533cdeb9ea41bb9e938900764fd6ad7
Rachel Suk <rachelsuk97@gmail.com>
2021-03-12 22:48:36 -0800

    Changes to settings

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rachelsuk/pyemr/venv"
+set -gx VIRTUAL_ENV "/Users/rachelsuk/codingprojects/pyemr/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
