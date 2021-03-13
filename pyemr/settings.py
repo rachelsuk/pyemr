7a003322ce5fafee52653b0c4b823f3752549806
Rachel Suk <rachelsuk97@gmail.com>
2021-03-12 22:31:52 -0800

    changes to settings part 2

--- pyemr/settings.py
+++ pyemr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, False))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
