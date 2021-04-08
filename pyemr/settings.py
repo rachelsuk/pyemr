39e610aea50056f751e9cf955842ccdd235e5c4d
Rachel Suk <rachelsuk97@gmail.com>
2021-04-07 18:48:45 -0700

    Added static.css file

--- pyemr/settings.py
+++ pyemr/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ptchart',
-    'research'
+    'research',
+    # 'pyemr'
 ]
 
 MIDDLEWARE = [
@@ -125,4 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
